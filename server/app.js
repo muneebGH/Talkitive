cc5b473ff098b9f096d3855b750b4075c8c4233e
muneebGH <muneeb.github@gmail.com>
2020-01-11 15:06:51 +0500

    brokemodularity 2

---
+++ server/app.js
@@ -0,0 +1,70 @@
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var bodyParser = require("body-parser");
+var userCRUD = require("./userHandling/CRUD");
+var loginValidation = require("./userHandling/loginValidation");
+var messageHandler = require("./messageHandling/holdMessages");
+
+var senderId = "123";
+var recieverId = "456";
+
+userCRUD.establishConnection();
+
+var path = require("path");
+
+app.use(express.static(__dirname.substring(0, __dirname.length - 6) + "/ui"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(3301, () => {
+  console.log("something happened");
+  console.log(__dirname.length);
+});
+
+io.on("connection", socket => {
+  console.log("socket io successfull");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./ui/screens/login_screen.html"));
+});
+
+app.get("/chat", (req, res) => {
+  res.sendFile(path.resolve("./ui/screens/chat_screen.html"));
+});
+
+app.get("/messages", async (req, res) => {
+  var res = await messageHandler.getAllOf(senderId, recieverId);
+  return res;
+});
+
+app.post("/messages", (req, res) => {
+  var done = messageHandler.addMessage(req.body);
+  io.emit("message", res.body);
+  done ? res.sendStatus(200) : res.sendStatus(500);
+});
+
+app.post("/adduser", (req, res) => {
+  console.log(req.body);
+  var failed = userCRUD.addUser(req.body);
+  if (failed) {
+    res.sendStatus(500);
+  }
+  //res.sendFile(path.resolve("./ui/screens/chat_screen.html"));
+  res.sendStatus(200);
+});
+
+app.get("/login", async (req, res) => {
+  var email = req.param("email");
+  var pass = req.param("password");
+  var pass = req.param("password");
+  var data = await loginValidation.findUser(email, pass);
+  if (data) {
+    console.log("data came to get login" + data);
+    res.status(200).send(data._id);
+  } else {
+    res.sendStatus(200);
+  }
+});
---
+++ ui/js/login_screen.js
@@ -0,0 +1,47 @@
+$(() => {
+  $("login_button").click(validateUser);
+  $("#create_account").click(setPageForCreateAccount);
+  $("login_button").click(validateUser);
+});
+
+function setPageForCreateAccount() {
+  //console.log("set page for create account called ");
+  $(".inputStuff").empty();
+  $("#login_button_div").remove();
+  $("#create_account").remove();
+  $("#dont_have_acc").text("Click to create account");
+
+  $(".inputStuff").append(createAccount);
+  $("#create_account_2").click(addUser);
+}
+
+function addUser() {
+  console.log(undefined);
+  console.log(1);
+  console.log(document.getElementById("full_name_ca").value);
+  console.log($("#full_name_ca").text());
+  var user = {
+    fullname: $("#full_name_ca").val(),
+    userName: $("#user_name_ca").val(),
+    email: $("#email_ca").val(),
+    password: $("#password_ca").val()
+  };
+
+  console.log(user.fullname);
+  $.post("http://localhost:3301/adduser", user);
+}
+
+function validateUser() {
+  user = {
+    email: $("#email_login").val(),
+    password: $("#password_login").val()
+  };
+
+  console.log("validate user called ");
+  $.get(
+    `http://localhost:3301/login?email=${user.email}&password=${user.password}`,
+    data => {
+      console.log("yes or no ?:" + data);
+    }
+  );
+}
